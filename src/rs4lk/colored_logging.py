8b330c4f59923e619f7b9cf2010c38473a5693a0
Mariano Scazzariello <marianoscazzariello@gmail.com>
2023-07-10 14:12:26 +0200

    Add Action4 + Logging refactoring

---
+++ src/rs4lk/colored_logging.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import logging
+from logging import Formatter, LogRecord
+
+import coloredlogs
+
+
+class Color:
+    colors = {
+        'red': 31,
+        'green': 32,
+        'yellow': 33,
+        'cyan': 36,
+        'white': 37,
+        'bgred': 41,
+        'bggrey': 100
+    }
+
+    prefix = '\033['
+
+    suffix = '\033[0m'
+
+    def colored(self, text: str, color: str | None = None) -> str:
+        if color not in self.colors:
+            color = 'white'
+
+        clr = self.colors[color]
+        return (self.prefix + '%dm%s' + self.suffix) % (clr, text)
+
+
+colored = Color().colored
+
+
+class ColoredFormatter(Formatter):
+    def format(self, record: LogRecord) -> str:
+        message = record.getMessage()
+
+        mapping = {
+            'DEBUG': 'bggrey',
+            'INFO': 'cyan',
+            'SUCCESS': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bgred',
+        }
+
+        clr = mapping.get(record.levelname, 'white')
+
+        return colored(record.levelname, clr) + ': ' + message
+
+
+def set_logging() -> None:
+    logging.SUCCESS = 25
+    logging.addLevelName(logging.SUCCESS, 'SUCCESS')
+    logging.success = lambda message, *args: logging.log(logging.SUCCESS, message, *args)
+
+    coloredlogs.install(fmt='%(levelname)-8s | %(message)s', level="INFO")
